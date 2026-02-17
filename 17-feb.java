752945ee61168348b5fe66377ca640a502c5aa5b
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2026-02-17 23:33:06 +0530

    Create 17-feb.java

---
+++ 17-feb.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public static int overlapInt(int[][] arr) {
+         int max_val= 0;
+         for (int[] interval :arr) {
+         max_val =Math.max(max_val,interval[1]);
+        }
+        int[] diff = new int[max_val+ 2];
+        for (int[] interval : arr) {
+             diff[interval[0]]++;
+             diff[interval[1] + 1]--;
+        }
+
+        int res = 0;
+        int current =0;
+
+        for (int i = 0; i <= max_val; i++) {
+            current += diff[i];
+        res = Math.max(res, current);
+        }
+
+        return res;
+    }
+}
