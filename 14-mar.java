bf8c5772b7c2ad0268db887f269410cd2c4dcf1e
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2026-03-14 18:48:03 +0530

    Add top view function for binary tree
    Implement top view of a binary tree using a queue and a map to track horizontal distances.

---
+++ 14-mar.java
@@ -0,0 +1,42 @@
+//Top View of Binary Tree
+class Solution {
+    
+    public ArrayList<Integer> topView(Node root) {
+        // code here
+      ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Queue<NodeHd> q = new LinkedList<>();
+        q.add(new NodeHd(root, 0));
+
+
+        Map<Integer, Integer> mp = new HashMap<>();
+        int minHd = Integer.MAX_VALUE;
+
+        while (!q.isEmpty()) {
+            NodeHd temp = q.poll();
+            Node node = temp.node;
+            int hd = temp.hd;
+            minHd = Math.min(minHd, hd);
+
+    
+            if (!mp.containsKey(hd)) {
+                mp.put(hd, node.data);
+            }
+
+            if (node.left != null) q.add(new NodeHd(node.left, hd - 1));
+            if (node.right != null) q.add(new NodeHd(node.right, hd + 1));
+        }
+
+        int maxHd = Integer.MIN_VALUE;
+        for (int key : mp.keySet()) {
+            maxHd = Math.max(maxHd, key);
+        }
+
+        for (int i = minHd; i <= maxHd; i++) {
+            ans.add(mp.get(i));
+        }
+
+        return ans;
+    }
+}
