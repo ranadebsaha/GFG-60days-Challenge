b3d4f357a7818f7129fc3184c7c7acedc5136045
Ranadeb Saha <103212375+ranadebsaha@users.noreply.github.com>
2026-03-20 19:49:48 +0530

    Implement findPreSuc method for predecessor and successor

---
+++ 20-mar.java
@@ -0,0 +1,44 @@
+//Predecessor and Successor
+class Solution {
+    static Node rightMost(Node node) {
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+   static Node leftMost(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    } 
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        // code here
+        Node pre = null, suc = null;
+        Node curr = root;
+
+        while (curr != null) {
+            if (curr.data < key) {
+                pre = curr;
+                
+                curr = curr.right;
+            } else if (curr.data > key) {
+                suc = curr;
+                
+                curr = curr.left;
+            } else {
+                if (curr.left != null)
+                    pre = rightMost(curr.left);
+                if (curr.right != null)
+                    suc = leftMost(curr.right);
+                break;
+            }
+        }
+
+        ArrayList<Node> result = new ArrayList<>();
+        result.add(pre);
+        result.add(suc);
+        return result;
+    }
+}
